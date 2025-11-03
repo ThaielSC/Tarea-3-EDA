452dc1ccb8f36cf361c0a653731d80da7baf8b8d
Thaiel Santiago <tmsantiago1@miuandes.cl>
2025-11-03 14:43:00 -0300

    Feature(Token): Difine Tokens types

--- include/lexer/token.hpp
+++ include/lexer/token.hpp
@@ -1,1 +1,45 @@
-class Token;
+#pragma once
+
+#include <string>
+
+enum class TokenType {
+  // Arithmetic Operators
+  PLUS,     // +
+  MINUS,    // -
+  MULTIPLY, // *
+  DIVIDE,   // /
+  POWER,    // ^
+  SQRT,     // sqrt
+
+  // Assignment
+  ASSIGN, // =
+
+  // Grouping
+  LPAREN, // (
+  RPAREN, // )
+
+  // Literals
+  NUMBER,     // e.g., 123, 3.14
+  IDENTIFIER, // e.g., ans, x, variable_name
+
+  // Keywords Commands
+  SHOW,
+  TREE,
+  POSFIX,
+  PREFIX,
+  EXIT,
+
+  // Special Tokens
+  EOF_TOKEN, // End of file/input
+  UNKNOWN    // For unrecognized tokens
+};
+
+struct Token {
+  std::string lexema;
+  TokenType type;
+
+  Token(TokenType type, const std::string &lexema)
+      : lexema(lexema), type(type) {}
+
+  Token(TokenType type) : lexema(""), type(type) {}
+};
